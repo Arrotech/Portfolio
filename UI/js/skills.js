70cd047522fc2e17b4e6300db61540d139e820d8
Arrotech <arrotechdesign@gmail.com>
2020-04-22 01:44:38 +0300

    Chore (Restructure Application Section)
    What does this pr do?
    
    Restructure application section

--- UI/js/skills.js
+++ UI/js/skills.js
@@ -1,5 +1,22 @@
-// Skills Bars
-$('.skill-percent').each(function(){
-  $(this).animate({
-    width:$(this).attr('data-percent')},"fast");
-  });
+function loadSkillgraph() {
+    $(".skillData").each(function(index, element) {
+        // element == this
+        var mydata = $(element).data();
+        var cnt = 0;
+
+        //recursive call with a time delay so user can see graph draw.
+        function go() {
+            if (cnt++ < mydata['percent']) {
+                setTimeout(go, 10);
+            }
+            $(element).css('width', cnt + '%');
+
+        }
+
+        go();
+
+    });
+
+}
+
+loadSkillgraph();
--- UI/js/slideshow.js
+++ UI/js/slideshow.js
@@ -1,4 +1,22 @@
-// // MDB Lightbox Init
-// $(function() {
-//     $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-// });
+// function loadSkillgraph() {
+//     $(".skillData").each(function(index, element) {
+//         // element == this
+//         var mydata = $(element).data();
+//         var cnt = 0;
+
+//         //recursive call with a time delay so user can see graph draw.
+//         function go() {
+//             if (cnt++ < mydata['percent']) {
+//                 setTimeout(go, 10);
+//             }
+//             $(element).css('width', cnt + '%');
+
+//         }
+
+//         go();
+
+//     });
+
+// }
+
+// loadSkillgraph();
